bdf5e5afd7fb3b9c37a968ee5a6d114361b8eeeb
qorxorbs <88699801+qorxorbs@users.noreply.github.com>
2023-04-05 16:14:57 +0900

    배열의 평균값
    문제 설명
    정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
    
    제한사항
    0 ≤ numbers의 원소 ≤ 1,000
    1 ≤ numbers의 길이 ≤ 100
    정답의 소수 부분이 .0 또는 .5인 경우만 입력으로 주어집니다.
    입출력 예
    numbers	result
    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]	5.5
    [89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]	94.0
    입출력 예 설명
    입출력 예 #1
    
    numbers의 원소들의 평균 값은 5.5입니다.
    입출력 예 #2
    
    numbers의 원소들의 평균 값은 94.0입니다.

---
+++ 디지털응용설계/16.py
@@ -0,0 +1,51 @@
+# 배열의 평균값
+# 문제 설명
+# 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+
+# 제한사항
+# 0 ≤ numbers의 원소 ≤ 1,000
+# 1 ≤ numbers의 길이 ≤ 100
+# 정답의 소수 부분이 .0 또는 .5인 경우만 입력으로 주어집니다.
+# 입출력 예
+# numbers	result
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]	5.5
+# [89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]	94.0
+# 입출력 예 설명
+# 입출력 예 #1
+
+# numbers의 원소들의 평균 값은 5.5입니다.
+# 입출력 예 #2
+
+# numbers의 원소들의 평균 값은 94.0입니다.
+
+
+#for문으로 리스트 첫번째 숫자부터 마지막 숫자까지 더하기
+# 다 더한 숫자 나누기 len(리스트)
+
+
+
+
+
+def solution(numbers):
+    k=0
+    for i in range(len(numbers)):
+        k=k+numbers[i]
+    answer = k/len(numbers)
+    return answer
+
+a=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ans=solution(a)
+print(ans)
+
+
+
+
+# import math
+# def solution(numbers):
+#     answer = sum(numbers)/len(numbers)
+#     return answer
+
+# a=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# ans=solution(a)
+# print(ans)
